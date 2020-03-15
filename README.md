78b6f7f330d121f4c312eeb9410c88388cc86921
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-03-15 16:26:59 -0700

    final code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Burger-app
+# Burger
+This is a simple burger logger app created with MySQL, Node, Express, Handlebars and a homemade ORM and have follow the MVC design pattern.
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Zen Chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sweet Chili', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+
